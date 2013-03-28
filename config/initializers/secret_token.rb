97c83f81f0e06770f63c9612263eaf295842b739
Chris <christopher.bon@hec.edu>
2013-03-28 18:41:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doozy::Application.config.secret_token = '3ad999b4a97904a89b297c45fd63a87d901b1b14e10a3faf7a91971f5dcc6cf388b5c1479387288792c99a9c581de1f2225d593c3eda16e82b4c5da46d78304c'
